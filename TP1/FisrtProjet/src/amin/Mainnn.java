d0b912737b3838b70e9d1997f8c07aa1f52bc356
Aminyayeh <kevindack0@gmail.com>
2022-04-26 17:07:17 +0300

    nouveaux mise a jour

--- TP1/FisrtProjet/src/amin/Mainnn.java
+++ TP1/FisrtProjet/src/amin/Mainnn.java
@@ -42,10 +42,10 @@ public class Mainnn {
 		no.setId_matière(ma);
 		System.out.println("Saisir le note 1");
 		float note1 = sc.nextFloat();
-		no.setMoyen_Semestre1(note1);
+		no.setNote1(note1);
 		System.out.println("Saisir le note 2");
 		float note2 = sc.nextFloat();
-		no.setMoyen_Semestre2(note2);
+		no.setNote2(note2);
 		
 		
 		Calculer(no , et , ma );
@@ -56,22 +56,22 @@ public class Mainnn {
 	public static void Calculer(Note no, Etudiant et , Matiere ma) {
 		
 		float moyen = 0 , somme = 0; 
-		float note1 = no.note1();
-		float note2 = no.note2();
+		float note1 = no.getNote1();
+		float note2 = no.getNote2();
 		somme = note1 + note2 ; 
 		moyen = somme/2;
 		if(moyen>=10) {
 			no.setResultat_Annee(moyen);
-			no.setAdmin("Admis");
+			no.setAdmis("Admis");
 		}
 		else {
 			no.setResultat_Annee(moyen);
-			no.setAdmin("Ajournée");
+			no.setAdmis("Ajournée");
 		}
 		System.out.println("------------------Affichage-----------------------------");
-		System.out.println("l'etudiant : "+et.getNom()+et.getPrenom()+"\nId matiere : "+ma.getNom()+"\nnote1 : "+no.getMoyen_Semestre1()+
-				"\nnote2 : "+no.getMoyen_Semestre2()+"\nMoyen general :"+no.getResultat_Annee()+
-				"\nResultat Année :"+no.getAdmin());
+		System.out.println("l'etudiant : "+et.getNom()+et.getPrenom()+"\nId matiere : "+ma.getNom()+"\nnote1 : "+no.getNote1()+
+				"\nnote2 : "+no.getNote2()+"\nMoyen general :"+no.getResultat_Annee()+
+				"\nResultat Année :"+no.getAdmis());
 
 	}
 
