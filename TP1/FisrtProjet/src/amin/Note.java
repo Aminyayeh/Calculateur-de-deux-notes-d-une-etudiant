fc8c7f7d59ee5d1b622ec4c5af3dec21d3e636e4
Aminyayeh <kevindack0@gmail.com>
2022-04-26 16:57:59 +0300

    mise a jour

--- TP1/FisrtProjet/src/amin/Note.java
+++ TP1/FisrtProjet/src/amin/Note.java
@@ -2,18 +2,18 @@ package amin;
 
 public class Note {
 	public Matiere Id_matière ;
-	public float Moyen_Semestre1;
-	public float Moyen_Semestre2 ;
-	public float Resultat_Annee ;
-	public String admin ;
+	public float note1;
+	public float note2 ;
+	public float moyen ;
+	public String admis ;
 	
 	public Note(Matiere id_matière, float moyen_Semestre1, float moyen_Semestre2, float resultat_Année, String admin) {
 		
 		Id_matière = id_matière;
-		Moyen_Semestre1 = moyen_Semestre1;
-		Moyen_Semestre2 = moyen_Semestre2;
-		Resultat_Annee = resultat_Année;
-		this.admin = admin;
+		note1 = moyen_Semestre1;
+		note2 = moyen_Semestre2;
+		moyen = resultat_Année;
+		this.admis = admin;
 	}
 	
 	public Note() {
@@ -29,35 +29,35 @@ public class Note {
 	}
 
 	public float getMoyen_Semestre1() {
-		return Moyen_Semestre1;
+		return note1;
 	}
 
 	public void setMoyen_Semestre1(float moyen_Semestre1) {
-		Moyen_Semestre1 = moyen_Semestre1;
+		note1 = moyen_Semestre1;
 	}
 
 	public float getMoyen_Semestre2() {
-		return Moyen_Semestre2;
+		return note2;
 	}
 
 	public void setMoyen_Semestre2(float moyen_Semestre2) {
-		Moyen_Semestre2 = moyen_Semestre2;
+		note2 = moyen_Semestre2;
 	}
 
 	public float getResultat_Annee() {
-		return Resultat_Annee;
+		return moyen;
 	}
 
 	public void setResultat_Annee(float resultat_Annee) {
-		Resultat_Annee = resultat_Annee;
+		moyen = resultat_Annee;
 	}
 
 	public String getAdmin() {
-		return admin;
+		return admis;
 	}
 
 	public void setAdmin(String admin) {
-		this.admin = admin;
+		this.admis = admin;
 	}
 
 	
